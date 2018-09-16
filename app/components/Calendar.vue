c7bb42b1956186d2149027403c23c9095031da7f
jan <git@sternprodukt.de>
2018-09-16 12:43:09 +0200

    add custom logo

--- app/components/Calendar.vue
+++ app/components/Calendar.vue
@@ -4,15 +4,54 @@
       <NavigationButton text="Go Back" android.systemIcon="ic_menu_back" @tap="$router.push('/home')"/>
     </ActionBar>
 
-    <RadCalendar id="calendar" />
+    <RadCalendar id="calendar"
+      :eventSource="calendarEvents" 
+
+      eventsViewMode="Inline" 
+      selectionMode="Single" 
+      viewMode="Month"
+      transitionMode="Stack" 
+      locale="de-DE"  />
+
 
   </Page>
 </template>
 
 <script>
   import { mapActions } from 'vuex';
+  //import Color from 'color';
+  const Color = require("color").Color;
+  const calendarModule = require("nativescript-ui-calendar");
+
 
   export default {
+
+    data() {
+      return {
+        calendarEvents: []
+      }
+    },
+
+    created() {
+      let events = [];
+      let now = new Date();
+      let startDate;
+      let endDate;
+      let event;
+      let colors = [new Color(200, 188, 26, 214), new Color(220, 255, 109, 130), new Color(255, 55, 45, 255), new Color(199, 17, 227, 10), new Color(255, 255, 54, 3)];
+      for (let i = 1; i < 10; i++) {
+        startDate = new Date(now.getFullYear(), now.getMonth(), i * 2, 1);
+        endDate = new Date(now.getFullYear(), now.getMonth(), (i * 2), 3);
+        event = new calendarModule.CalendarEvent("event " + i, startDate, endDate, false, colors[i * 10 % (colors.length - 1)]);
+        events.push(event);
+        if (i % 3 == 0) {
+          event = new calendarModule.CalendarEvent("second " + i, startDate, endDate, true, colors[i * 5 % (colors.length - 1)]);
+          events.push(event);
+        }
+      }
+      this.calendarEvents = events;
+    },
+
     computed: {
       message () {
         return this.$store.state.counter.count.toString();
