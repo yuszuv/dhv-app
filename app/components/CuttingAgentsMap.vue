5c358e44f94b776116f60bc5f860fdd8a3917a1a
jan <git@sternprodukt.de>
2018-09-17 15:34:22 +0200

    get cutting agent data via http

--- app/components/CuttingAgentsMap.vue
+++ app/components/CuttingAgentsMap.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import * as utils from "utils/utils";
+import * as http from 'http';
 
 export default {
   data () {
@@ -37,40 +37,20 @@ export default {
     }
   },
 
-  created() {
-    this.markers = [
-      {
-        id: 1,
-        lat: 53.165190,
-        lng: 12.486542,
-        title: "16.09.18: Rahmstufe",
-        subtitle: "Home of The Polyglot Developer!",
-        onCalloutTap: () => {
-          this.$router.push('/cutting-agent-alerts/1');
-        }
-      },
-      {
-        id: 2,
-        lat: 53.131695,
-        lng: 12.513761,
-        title: "15.09.18: Burg Goldbeck",
-        subtitle: "Home of The Polyglot Developer!",
-        onCalloutTap: () => {
-          this.$router.push('/cutting-agent-alerts/2');
-        }
-      },
-      {
-        id: 3,
-        lat: 53.163232,
-        lng: 12.485249,
-        title: "04.05.18: Marktplatz Wittstock",
-        subtitle: "Home of The Polyglot Developer!",
-        onCalloutTap: () => {
-          this.$router.push('/cutting-agent-alerts/3');
-        }
-      },
-    ]
-  }
+  mounted() {
+    fetch("https://dhv-api.sternprodukt.de/cutting-agent-alerts", {
+      headers: { 'Authorization': 'Bearer GO13TNBbf5jJ6Ts8xf64d8GetfpnLSaj' }
+    })
+      .then(response => response.json(), error => console.log(error))
+      .then(data => {
+        data.map(json => {
+          this.markers.push({ ...json, onCalloutTap: () =>
+            { this.$router.push(`/cutting-agent-alerts/${json.id}`) } });
+        })
+      })
+
+  },
+
 };
 </script>
 
